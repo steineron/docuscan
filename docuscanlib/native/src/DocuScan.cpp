fbc6d11ebe442b7cfec6b6e6408051692cbdce01
Ron Steiner <ron.steiner@domain.com.au>
2019-11-26 18:19:42 +1100

    setup include folders

--- docuscanlib/native/src/DocuScan.cpp
+++ docuscanlib/native/src/DocuScan.cpp
@@ -4,7 +4,14 @@
 
 #include <jni.h>
 #include <string>
+#include <iostream>
+#include "opencv2/core/utility.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
 
+using namespace std;
+using namespace cv;
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_locii_docuscanlib_DocuScan_stringFromJNI(
@@ -15,3 +22,27 @@ Java_com_locii_docuscanlib_DocuScan_stringFromJNI(
 }
 
 
+
+
+/**
+ * find the closesest point to @param p in @param points by computing
+ * the minimum Euclidean Distance between the point p and any of the other points
+ * @param p
+ * @param points
+ * @return the point in points closest to p
+ */
+static Point2f closestPoint(Point &p, Point2f points[], int nPoints){
+
+    int minDistance = INT_MAX;
+    Point2f result;
+    for(int i=0; i<nPoints; i++){
+        Point2f candidate = points[i];
+        int distance = pow(candidate.x - p.x, 2) + pow(candidate.y - p.y, 2);
+        if(distance < minDistance){
+            minDistance=distance;
+            result=candidate;
+        }
+    }
+
+    return result;
+}
--- docuscanlib/src/androidTest/java/com/locii/docuscanlib/InstDocuScanTest.kt
+++ docuscanlib/src/androidTest/java/com/locii/docuscanlib/InstDocuScanTest.kt
@@ -1,10 +1,13 @@
 package com.locii.docuscanlib
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Before
 
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class InstDocuScanTest {
 
     lateinit var docuScan: DocuScan
