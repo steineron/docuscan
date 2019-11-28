d1c99a77814b0a92cb48d4312899da628f8abbe6
Ron Steiner <ron.steiner@domain.com.au>
2019-11-28 18:07:24 +1100

    rename module

--- docuscanlib/src/main/java/com/locii/docuscanlib/DocuScan.kt
+++ docuscanlib/src/main/java/com/locii/docuscanlib/DocuScan.kt
@@ -3,18 +3,17 @@ package com.locii.docuscanlib
 class DocuScan{
 
     /**
-     * A native method that is implemented by the 'opencv_jni_shared' native library,
+     * A native method that is implemented by the 'docuscan_jni_shared' native library,
      * which is packaged with this module
      *calls Java_com_locii_docuscanlib_DocuScan_stringFromJNI.
      */
     external fun stringFromJNI(): String
 
-
     companion object {
 
         // Used to load the 'native-lib' library on application startup.
         init {
-            System.loadLibrary("opencv_jni_shared")
+            System.loadLibrary("docuscan_jni_shared")
         }
     }
 }
